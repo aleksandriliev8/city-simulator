c2ea7755f2d9bc6bc4199df2c38b2af5f2ce4d12
Aleksandar Iliev <aleksandriliev8@gmail.com>
2026-06-24 19:40:56 +0300

    Created HistoryEntry

--- main/src/core/HistoryEntry/HistoryEntry.cpp
+++ main/src/core/HistoryEntry/HistoryEntry.cpp
@@ -1,1 +1,17 @@
-#pragma once
+#include "HistoryEntry.hpp"
+
+HistoryEntry::HistoryEntry(const Date& date, const std::string& description)
+    : date(date), description(description) {
+}
+
+const Date& HistoryEntry::getDate() const {
+    return date;
+}
+
+const std::string& HistoryEntry::getDescription() const {
+    return description;
+}
+
+std::string HistoryEntry::toString() const {
+    return date.toString() + " - " + description;
+}
