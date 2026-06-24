6bf4293ccf86085cacdbd34485cf701c17a99fbb
Aleksandar Iliev <aleksandriliev8@gmail.com>
2026-06-24 19:01:11 +0300

    Created PanelBuilding

--- main/src/buildings/PanelBuilding/PanelBuilding.cpp
+++ main/src/buildings/PanelBuilding/PanelBuilding.cpp
@@ -1,1 +1,24 @@
-#pragma once
+#include "PanelBuilding.hpp"
+
+PanelBuilding::PanelBuilding(int row, int col) : Building(row, col) {
+}
+
+std::string PanelBuilding::getType() const {
+    return "Panel";
+}
+
+double PanelBuilding::getBaseRent() const {
+    return 500.0;
+}
+
+int PanelBuilding::getCapacity() const {
+    return 100;
+}
+
+bool PanelBuilding::canHouseStudent() const {
+    return false;
+}
+
+Building* PanelBuilding::clone() const {
+    return new PanelBuilding(*this);
+}
