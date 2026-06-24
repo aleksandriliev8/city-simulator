de1a6a9261fbe658f0d6877528b0750b3b36c06d
Aleksandar Iliev <aleksandriliev8@gmail.com>
2026-06-24 19:02:19 +0300

    Created Dormitory

--- main/src/buildings/Dormitory/Dormitory.cpp
+++ main/src/buildings/Dormitory/Dormitory.cpp
@@ -1,1 +1,24 @@
-#pragma once
+#include "Dormitory.hpp"
+
+Dormitory::Dormitory(int row, int col) : Building(row, col) {
+}
+
+std::string Dormitory::getType() const {
+    return "Dormitory";
+}
+
+double Dormitory::getBaseRent() const {
+    return 67.0;
+}
+
+int Dormitory::getCapacity() const {
+    return 200;
+}
+
+bool Dormitory::canHouseStudent() const {
+    return true;
+}
+
+Building* Dormitory::clone() const {
+    return new Dormitory(*this);
+}
